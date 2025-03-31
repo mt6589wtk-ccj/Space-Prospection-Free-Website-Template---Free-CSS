3b9378269bde76f8bebf92bb7273a4f508516274
當機等待網頁 <D1397009@o365.fcu.edu.tw>
2025-03-31 18:39:32 +0800

    圖片壓縮

--- js/mobile.js
+++ js/mobile.js
@@ -1,22 +1,22 @@
 window.onload = function(){
-	var getNavi = document.getElementById('navigation');
+	let getNavi = document.getElementById('navigation');
 
-	var mobile = document.createElement("span");
+	let mobile = document.createElement("span");
 	mobile.setAttribute("id","mobile-navigation");
 	getNavi.parentNode.insertBefore(mobile,getNavi);
 
 	document.getElementById('mobile-navigation').onclick = function(){
-		var a = getNavi.getAttribute('style');
+		let a = getNavi.getAttribute('style');
 		if(a){
 			getNavi.removeAttribute('style');
-			document.getElementById('mobile-navigation').style.backgroundImage='url(/space-science/upload/images/mobile/mobile-menu.png)';
+			document.getElementById('mobile-navigation').style.backgroundImage='url(/images/mobile/mobile-menu.png)';
 		} else {
 			getNavi.style.display='block';
-			document.getElementById('mobile-navigation').style.backgroundImage='url(/space-science/upload/images/mobile/mobile-close.png)';
+			document.getElementById('mobile-navigation').style.backgroundImage='url(/images/mobile/mobile-close.png)';
 		}
 	};
-	var getElm = getNavi.getElementsByTagName("LI");
-	for(var i=0;i<getElm.length;i++){
+	let getElm = getNavi.getElementsByTagName("LI");
+	for(let i=0;i<getElm.length;i++){
 		if(getElm[i].children.length>1){
 			var smenu = document.createElement("span");
 			smenu.setAttribute("class","mobile-submenu");
@@ -25,15 +25,15 @@ window.onload = function(){
 		};
 	};
 	submenu = function (i){
-		var sub = getElm[i].children[1];
-		var b = sub.getAttribute('style');
+		let sub = getElm[i].children[1];
+		let b = sub.getAttribute('style');
 		if(b){
 			sub.removeAttribute('style');
-			getElm[i].lastChild.style.backgroundImage='url(/space-science/upload/images/mobile/mobile-expand.png)';
+			getElm[i].lastChild.style.backgroundImage='url(/images/mobile/mobile-expand.png)';
 			getElm[i].lastChild.style.backgroundColor='rgba(98, 0, 49, 0.91)';
 		} else {
 			sub.style.display='block';
-			getElm[i].lastChild.style.backgroundImage='url(/space-science/upload/images/mobile/mobile-collapse.png)';
+			getElm[i].lastChild.style.backgroundImage='url(/images/mobile/mobile-collapse.png)';
 			getElm[i].lastChild.style.backgroundColor='rgba(0, 0, 0, 0.91)';
 		}
 	};
